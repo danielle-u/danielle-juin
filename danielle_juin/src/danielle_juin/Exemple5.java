894289dd8486cbeb03ec9b95a39621bf8eb64675
dani <dani@192.168.2.18>
2021-06-11 10:39:17 -0400

    06/11/21

--- danielle_juin/src/danielle_juin/Exemple5.java
+++ danielle_juin/src/danielle_juin/Exemple5.java
@@ -6,11 +6,25 @@
 
 package danielle_juin;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Exemple5 {
 
 	public static void main(String[] args) throws IOException, FileNotFoundException
 	{
-		// TODO Auto-generated method stub
+		BufferedReader entree;
+		String ligne;
+		entree = new BufferedReader (new FileReader ("monFichier.txt"));
+		while(entree.ready())
+			
+		{
+			ligne=entree.readLine();
+			System.out.println(ligne);
+		}
+		entree.close();
 
 	}
 
---
+++ danielle_juin/src/danielle_juin/monFichier.java
@@ -0,0 +1,16 @@
+/*
+* Author : dani
+* Date : Jun. 11, 2021
+* Description :
+*/
+
+package danielle_juin;
+
+public class monFichier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
